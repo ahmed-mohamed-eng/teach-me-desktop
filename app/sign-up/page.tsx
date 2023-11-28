693e8e88f575aa4946d1dc2bc564eaa3f701415b
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2023-11-28 15:58:53 +0200

    refactory the home and signup pages

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import LoginButton from "@/components/pages/singUp/LoginButton";
+import RegisterComp from "@/components/pages/singUp/RegisterComp";
+
+export default function SignUp() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (localStorage && localStorage.getItem("adminToken")) {
+      router.push("/");
+    }
+  }, [router]);
+
+  return (
+    <main className="w-full bg-orange-500 flex min-h-screen flex-col items-center justify-start py-10 px-24">
+      <h1 className="text-5xl text-white font-semibold font-cairo mb-7">
+        Welcome to Our System &#x1F601;
+      </h1>
+      <section className="p-10 w-full h-full bg-white rounded-xl shadow-xl shadow-gray-900 flex flex-col items-center justify-start space-y-5">
+        <h2 className="text-4xl font-cairo font-bold">Register Now</h2>
+        <RegisterComp />
+        <LoginButton />
+      </section>
+    </main>
+  );
+}
