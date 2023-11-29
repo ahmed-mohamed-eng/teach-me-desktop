cd38c35c5cba4eb244d1ec48ad51f9df16013eb3
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2023-11-29 16:47:44 +0200

    updated the header

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/pages/home/Header";
+
+const ProfilePage = () => {
+  return (
+    <main className="w-full bg-orange-500 flex min-h-screen flex-col items-start justify-start p-5 space-y-5">
+      <Header />
+    </main>
+  );
+};
+
+export default ProfilePage;
