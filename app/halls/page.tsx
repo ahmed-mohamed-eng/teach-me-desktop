54d3ee3dee9774fa81f415494df74ff7d16483f5
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2023-11-30 15:50:49 +0200

    added the halls page

--- app/halls/page.tsx
+++ app/halls/page.tsx
@@ -2,17 +2,17 @@
 
 import Header from "@/components/pages/home/Header";
 import PaginationComp from "@/components/shared/PaginationComp";
-import SearchFilters from "@/components/pages/centers/SearchFilters";
+import SearchFilters from "@/components/pages/halls/SearchFilters";
+import CentersShowCards from "@/components/pages/halls/CentersShowCards";
 import FloatingCreateButton from "@/components/shared/FloatingCreateButton";
-import CentersShowCards from "@/components/pages/centers/CentersShowCards";
 
 const CentersPage = () => {
   return (
     <main className="w-full bg-orange-500 flex min-h-screen flex-col items-start justify-start p-5 space-y-5">
-      <Header title="Centers Page" />
+      <Header title="Halls Page" />
       <SearchFilters />
       <CentersShowCards />
-      <FloatingCreateButton linkTo="/centers/create" />
+      <FloatingCreateButton linkTo="/halls/create" />
       <PaginationComp />
     </main>
   );
--- components/pages/halls/SearchFilters/index.tsx
+++ components/pages/halls/SearchFilters/index.tsx
@@ -5,7 +5,31 @@ const SearchFilters = () => {
   return (
     <section className="w-full bg-white p-3 flex items-center justify-start rounded-lg shadow-xl space-x-5">
       <TextSearch />
-      <OptionsSearch options={[]} />
+      <OptionsSearch
+        htmlName="center-name"
+        placeholder="Select Center"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="admin-name"
+        placeholder="Select Admin"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="creation-year"
+        placeholder="Select Creation Year"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="creation-month"
+        placeholder="Select Creation Month"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="hall-status"
+        placeholder="Select Status"
+        options={[]}
+      />
     </section>
   );
 };
