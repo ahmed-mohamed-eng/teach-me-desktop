de138179af131d53569be31fddac6549cc3cbe2e
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2023-11-30 16:10:48 +0200

    added the teachers page

--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -2,17 +2,17 @@
 
 import Header from "@/components/pages/home/Header";
 import PaginationComp from "@/components/shared/PaginationComp";
-import SearchFilters from "@/components/pages/centers/SearchFilters";
+import SearchFilters from "@/components/pages/teachers/SearchFilters";
 import FloatingCreateButton from "@/components/shared/FloatingCreateButton";
-import CentersShowCards from "@/components/pages/centers/CentersShowCards";
+import CentersShowCards from "@/components/pages/teachers/CentersShowCards";
 
 const CentersPage = () => {
   return (
     <main className="w-full bg-orange-500 flex min-h-screen flex-col items-start justify-start p-5 space-y-5">
-      <Header title="Centers Page" />
+      <Header title="Teachers Page" />
       <SearchFilters />
       <CentersShowCards />
-      <FloatingCreateButton linkTo="/centers/create" />
+      <FloatingCreateButton linkTo="/teachers/create" />
       <PaginationComp />
     </main>
   );
--- components/pages/teachers/SearchFilters/index.tsx
+++ components/pages/teachers/SearchFilters/index.tsx
@@ -4,8 +4,32 @@ import OptionsSearch from "./OptionsSearch";
 const SearchFilters = () => {
   return (
     <section className="w-full bg-white p-3 flex items-center justify-start rounded-lg shadow-xl space-x-5">
-      <TextSearch />
-      <OptionsSearch options={[]} />
+      <TextSearch name="Search Teacher Name" />
+      <OptionsSearch
+        htmlName="creator-admin"
+        placeholder="Select Creator Admin"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="parent-year"
+        placeholder="Select Teacher Year"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="parent-month"
+        placeholder="Select Teacher Month"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="parent-state"
+        placeholder="Select Teacher State"
+        options={[]}
+      />
+      <OptionsSearch
+        htmlName="parent-location"
+        placeholder="Select Teacher Location"
+        options={[]}
+      />
     </section>
   );
 };
