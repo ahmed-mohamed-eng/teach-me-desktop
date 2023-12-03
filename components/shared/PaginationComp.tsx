b03377f5705b85877f660c95fcf7d814b2c6b9b2
Ahmed Karmy <ahmed.mohamed.eng05@gmail.com>
2023-12-03 18:30:33 +0200

    fixed the position of the paginations

--- components/shared/PaginationComp.tsx
+++ components/shared/PaginationComp.tsx
@@ -4,7 +4,7 @@ import ReactPaginate from "react-paginate";
 
 const PaginationComp = () => {
   return (
-    <div className="fixed bottom-7 left-[35%] w-96 h-12">
+    <div className="fixed bottom-7 left-0 right-0 my-0 mx-auto w-96 h-12">
       <ReactPaginate
         containerClassName="flex items-center justify-between bg-blue-700 p-3 rounded-md"
         nextClassName="text-white"
